58e414febdf55ecf19c80e9253e1c97ee85d9f8d
Daniel Roberts <daniel.prescott.roberts@gmail.com>
2021-01-26 15:08:58 -0500

    Add project files.

---
+++ IT391_Roberts_Unit6/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace IT391_Roberts_Unit6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtMiddleInitial = new System.Windows.Forms.TextBox();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblMiddleInitial = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.txtEmailAddress = new System.Windows.Forms.TextBox();
+            this.lblEmailAddress = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblZipCode = new System.Windows.Forms.Label();
+            this.txtStreetAddress = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.txtZipCode = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtRating = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblRequiredFields = new System.Windows.Forms.Label();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(286, 73);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(210, 31);
+            this.txtFirstName.TabIndex = 1;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(286, 206);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(210, 31);
+            this.txtLastName.TabIndex = 3;
+            // 
+            // txtMiddleInitial
+            // 
+            this.txtMiddleInitial.Location = new System.Drawing.Point(286, 142);
+            this.txtMiddleInitial.MaxLength = 1;
+            this.txtMiddleInitial.Name = "txtMiddleInitial";
+            this.txtMiddleInitial.Size = new System.Drawing.Size(70, 31);
+            this.txtMiddleInitial.TabIndex = 2;
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(92, 76);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(130, 25);
+            this.lblFirstName.TabIndex = 4;
+            this.lblFirstName.Text = "First Name *";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(92, 209);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(129, 25);
+            this.lblLastName.TabIndex = 5;
+            this.lblLastName.Text = "Last Name *";
+            // 
+            // lblMiddleInitial
+            // 
+            this.lblMiddleInitial.AutoSize = true;
+            this.lblMiddleInitial.Location = new System.Drawing.Point(92, 145);
+            this.lblMiddleInitial.Name = "lblMiddleInitial";
+            this.lblMiddleInitial.Size = new System.Drawing.Size(132, 25);
+            this.lblMiddleInitial.TabIndex = 6;
+            this.lblMiddleInitial.Text = "Middle Initial";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(92, 290);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(50, 25);
+            this.lblAge.TabIndex = 10;
+            this.lblAge.Text = "Age";
+            // 
+            // txtEmailAddress
+            // 
+            this.txtEmailAddress.Location = new System.Drawing.Point(286, 704);
+            this.txtEmailAddress.Name = "txtEmailAddress";
+            this.txtEmailAddress.Size = new System.Drawing.Size(351, 31);
+            this.txtEmailAddress.TabIndex = 11;
+            // 
+            // lblEmailAddress
+            // 
+            this.lblEmailAddress.Location = new System.Drawing.Point(93, 704);
+            this.lblEmailAddress.Name = "lblEmailAddress";
+            this.lblEmailAddress.Size = new System.Drawing.Size(185, 38);
+            this.lblEmailAddress.TabIndex = 12;
+            this.lblEmailAddress.Text = "Email Address *";
+            this.lblEmailAddress.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(92, 383);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 32);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Street Address";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(93, 459);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 32);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "City";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(93, 533);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 32);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "State";
+            // 
+            // lblZipCode
+            // 
+            this.lblZipCode.Location = new System.Drawing.Point(93, 613);
+            this.lblZipCode.Name = "lblZipCode";
+            this.lblZipCode.Size = new System.Drawing.Size(160, 32);
+            this.lblZipCode.TabIndex = 16;
+            this.lblZipCode.Text = "Zip Code *";
+            // 
+            // txtStreetAddress
+            // 
+            this.txtStreetAddress.Location = new System.Drawing.Point(286, 384);
+            this.txtStreetAddress.Name = "txtStreetAddress";
+            this.txtStreetAddress.Size = new System.Drawing.Size(351, 31);
+            this.txtStreetAddress.TabIndex = 7;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(286, 460);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(210, 31);
+            this.txtCity.TabIndex = 8;
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(286, 534);
+            this.txtState.MaxLength = 2;
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(54, 31);
+            this.txtState.TabIndex = 9;
+            // 
+            // txtZipCode
+            // 
+            this.txtZipCode.Location = new System.Drawing.Point(286, 610);
+            this.txtZipCode.MaxLength = 5;
+            this.txtZipCode.Name = "txtZipCode";
+            this.txtZipCode.Size = new System.Drawing.Size(122, 31);
+            this.txtZipCode.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(93, 805);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(185, 144);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "On a scale of 1 (Worst) to 10 (Best), how would you rate this survey?";
+            // 
+            // txtRating
+            // 
+            this.txtRating.Location = new System.Drawing.Point(286, 855);
+            this.txtRating.MaxLength = 2;
+            this.txtRating.Name = "txtRating";
+            this.txtRating.Size = new System.Drawing.Size(95, 31);
+            this.txtRating.TabIndex = 12;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(286, 978);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(188, 44);
+            this.btnSubmit.TabIndex = 13;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblRequiredFields
+            // 
+            this.lblRequiredFields.Location = new System.Drawing.Point(107, 1045);
+            this.lblRequiredFields.Name = "lblRequiredFields";
+            this.lblRequiredFields.Size = new System.Drawing.Size(528, 61);
+            this.lblRequiredFields.TabIndex = 25;
+            this.lblRequiredFields.Text = "**All fields marked with an asterisk (*) are required.**";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(286, 287);
+            this.txtAge.MaxLength = 3;
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(54, 31);
+            this.txtAge.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(732, 1129);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.lblRequiredFields);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtRating);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtZipCode);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtStreetAddress);
+            this.Controls.Add(this.lblZipCode);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblEmailAddress);
+            this.Controls.Add(this.txtEmailAddress);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblMiddleInitial);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.txtMiddleInitial);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Name = "Form1";
+            this.Text = "Survey";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtMiddleInitial;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblMiddleInitial;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.TextBox txtEmailAddress;
+        private System.Windows.Forms.Label lblEmailAddress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblZipCode;
+        private System.Windows.Forms.TextBox txtStreetAddress;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.TextBox txtZipCode;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtRating;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblRequiredFields;
+        private System.Windows.Forms.TextBox txtAge;
+    }
+}
+
---
+++ IT391_Roberts_Unit6/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IT391_Roberts_Unit6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            bool ValidationTest = true;
+            string ErrorMessages = "";
+
+            //Check for Presence of Values
+            if (txtFirstName.TextLength == 0)
+            {
+                ErrorMessages += "You must enter your First Name.\n";
+                ValidationTest = false;
+            }
+            if (txtLastName.TextLength == 0)
+            {
+                ErrorMessages += "You must enter your Last Name.\n";
+                ValidationTest = false;
+            }
+            if (txtZipCode.TextLength == 0)
+            {
+                ErrorMessages += "You must enter your Zip Code.\n";
+                ValidationTest = false;
+            }
+            if (txtEmailAddress.TextLength == 0)
+            {
+                ErrorMessages += "You must enter your Email Address.\n";
+                ValidationTest = false;
+            }
+
+            //Check for proper Value Length
+            if (txtFirstName.TextLength > 20)
+            {
+                ErrorMessages += "Entered First Name has too many characters (limit 20).\n";
+                ValidationTest = false;
+            }
+            if (txtLastName.TextLength > 20)
+            {
+                ErrorMessages += "Entered Last Name has too many characters (limit 20).\n";
+                ValidationTest = false;
+            }
+            if (txtStreetAddress.TextLength > 50)
+            {
+                ErrorMessages += "Entered Street Address has too many characters (limit 50).\n";
+                ValidationTest = false;
+            }
+            if (txtCity.TextLength > 30)
+            {
+                ErrorMessages += "Entered City has too many characters (limit 30)\n";
+                ValidationTest = false;
+            }
+            if (txtState.TextLength != 2 && txtState.TextLength != 0)
+            {
+                ErrorMessages += "Entered State has the wrong number of characters (must be 2)\n";
+                ValidationTest = false;
+            }
+            if (txtZipCode.TextLength != 5 && txtZipCode.TextLength != 0)
+            {
+                ErrorMessages += "Invalid Zip Code. Required length is 5 characters.\n";
+                ValidationTest = false;
+            }
+
+            //Check for Valid State
+            foreach (char s in txtState.Text)
+            {
+                if (!Char.IsLetter(s))
+                {
+                    ErrorMessages += "Invalid State. Please enter correct 2 letter State Code\n";
+                    ValidationTest = false;
+                    break;
+                }
+            }
+
+            //Check for Valid Zip Code (Numerals Only)
+            foreach (char z in txtZipCode.Text)
+            {
+                if (z < '0' || z > '9')
+                {
+                    ErrorMessages += "Invalid Zip Code. Zip Code must only contain numerals.\n";
+                    ValidationTest = false;
+                    break;
+                }
+            }
+
+            //Check for Valid Email Address
+            string FullEmailAddress, EmailLocal, EmailDomain;
+            string[] SplitEmailAddress;
+            FullEmailAddress = txtEmailAddress.Text;
+            if ((FullEmailAddress.LastIndexOf("@") == FullEmailAddress.IndexOf("@")) && FullEmailAddress.IndexOf("@") != -1)
+            {
+                SplitEmailAddress = FullEmailAddress.Split('@');
+                EmailLocal = SplitEmailAddress[0];
+                EmailDomain = SplitEmailAddress[1];
+                if (EmailLocal.Length > 64)
+                {
+                    ErrorMessages += "Entered Email Local Recipient Name has too many characters (limit 64)\n";
+                    ValidationTest = false;
+                }
+                if (EmailDomain.Length > 255)
+                {
+                    ErrorMessages += "Entered Email Domain Recipient Name has too many characters (limit 255)\n";
+                    ValidationTest = false;
+                }
+                if (FullEmailAddress.Length > 320)
+                {
+                    ErrorMessages += "Entered Email Address has too many characters (limit 320)\n";
+                    ValidationTest = false;
+                }
+
+                //Check for Valid Domain
+                if (FullEmailAddress.IndexOf(".") == -1)
+                {
+                    ErrorMessages += "Entered Email Domain is invalid. Please confirm correct domain.\n";
+                    ValidationTest = false;
+                }
+            }
+            else if (txtEmailAddress.Text.Length > 0)
+            {
+                ErrorMessages += "You have entered an invalid Email Address.\n";
+                ValidationTest = false;
+            }
+
+            //Check for Valid Survey Rating
+            if (txtRating.TextLength > 0)
+            {
+                if (Int32.TryParse(txtRating.Text, out int rating))
+                {
+                    if (rating < 1 || rating > 10)
+                    {
+                        ErrorMessages += "You have entered an invalid Rating. Please enter a numeric value between 1 and 10.\n";
+                        ValidationTest = false;
+                    }
+                }
+                else
+                {
+                    ErrorMessages += "You have entered an invalid Rating. Please enter a numeric value between 1 and 10.\n";
+                    ValidationTest = false;
+                }
+            }
+
+            //Display MessageBox based on Validation Results
+            if (ValidationTest == true)
+            {
+                //Declare and Assign form values to their program variable equivalents
+                //Declare Variables for Submission
+                string FirstName = txtFirstName.Text;
+                string MiddleInitial = txtMiddleInitial.Text;
+                string LastName = txtLastName.Text;
+                //Only assign value if txtAge is not empty
+                if (txtAge.TextLength > 0)
+                {
+                    int Age = Int32.Parse(txtAge.Text);
+                }
+                string StreetAddress = txtStreetAddress.Text;
+                string City = txtCity.Text;
+                string State = txtState.Text;
+                if (txtZipCode.TextLength > 0)
+                {
+                    int ZipCode = Int32.Parse(txtZipCode.Text);
+                }
+                string EmailAddress = txtEmailAddress.Text;
+                if (txtRating.TextLength > 0)
+                {
+                    int Rating = Int32.Parse(txtRating.Text);
+                }
+
+                //Confirm to user that data was validated and submitted successfully.
+                MessageBox.Show("Data validated and submitted successfully!");
+                //Clear contents of form after successful submission
+                foreach (Control c in this.Controls)
+                {
+                    if (c.GetType() == typeof(TextBox))
+                    {
+                        c.Text = "";
+                    }
+                }
+            }
+            else
+            {
+                //Show Error Messages
+                MessageBox.Show(ErrorMessages);
+            }
+        }
+    }
+}
